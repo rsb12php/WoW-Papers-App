1f3e197acbd767c597da3d1f0f9c08d2d7d6b8a8
rb-br <49247622+rb-br@users.noreply.github.com>
2019-05-06 21:57:40 -0300

    Update environment.ts
    Atualização-Banco-Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB6gqAEH4BQ6n78r0djWFvXGl_gwqDjvR8",
+    authDomain: "wow-papers.firebaseapp.com",
+    databaseURL: "https://wow-papers.firebaseio.com",
+    projectId: "wow-papers",
+    storageBucket: "wow-papers.appspot.com",
+    messagingSenderId: "809906569357",
+    appId: "1:809906569357:web:ab047204b108f87f"
+  }
 };
 
 /*
